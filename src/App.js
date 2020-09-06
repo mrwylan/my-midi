7cfe09139c95fe5e526b18aa7b695f21a9b645b8
Jakob Albrecht <mrwylan@gmail.com>
2020-09-06 20:42:03 +0200

    some progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
+import note from './note.svg';
 import './App.css';
+import Box from './Box.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return (<div className="App" >
+    <header className="App-header" >
+      <Box title="MIDI in your browser" isVerified={navigator.requestMIDIAccess}>
+      <img id="clickable"
+        src={note}
+        className="Note"
+        alt="note"
+        onClick={ (e) => { console.log(e.target.classList);
+        e.target.classList.toggle("Note-Selected"); } } />
+      </Box>
+      <img src={logo}
+        className="App-logo"
+        alt="logo"
+         />
+      <p>
+        Edit 
+        <code> src / App.js </code> and save to reload. </p> 
+        <a className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer" >
+            Learn React </a> 
+            <Box title="Debug"></Box>
+    </header>    
+        </div>
+    );
 }
 
 export default App;
