1ba6b9be3d9995d024909ddeb10ce323e60ed2e5
Jakob Albrecht <mrwylan@gmail.com>
2020-09-19 16:36:59 +0200

    added midi out f()

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import { ChannelNote, ChannelScale, SemitoneSteps } from './ChannelBox';
 type AppState = {
   midiIn: Input[],
   midiOut: Output[],
-  noteOn: InputEventNoteon | null
+  noteOn: InputEventNoteon | null,
+  output: Output | null
 }
 
 function App() {
@@ -18,12 +19,14 @@ function App() {
   const initialState : AppState= {
     midiIn: [],
     midiOut: [],
-    noteOn: null
+    noteOn: null,
+    output: null
   };
 
   const [ourState, ourSetState] = useState(initialState);
   const { midiIn, midiOut } = ourState;
   const noteOnListener = (e : InputEventNoteon) => { ourSetState({ ...ourState, noteOn: e})};
+  const outputSelection = (e: Output | null) => { ourSetState({ ...ourState, output: e})};
 
   WebMidi.enable(function (err) {
 
@@ -42,7 +45,7 @@ function App() {
   return (<div className="App" >
     
     <header className="App-header" >
-      <Box title="MIDI in your browser" isVerified={undefined !== navigator.requestMIDIAccess}>
+      <Box title="MIDI IN" isVerified={undefined !== navigator.requestMIDIAccess}>
 
         { Array.from(midiIn.values(), (value, key) => {
            const name : string = value.name;
@@ -62,21 +65,52 @@ function App() {
          })}
 
       </Box>
+
+      <Box title="MIDI OUT" isVerified={undefined !== navigator.requestMIDIAccess}>
+
+        { Array.from(midiOut.values(), (value, key) => {
+           const name : string = value.name;
+           return       <div key={key + "x" + value.id} className="tooltip"><img id={value.id}
+           src={note}
+           className="Note"
+           alt="note"
+           onClick={ (e) => { 
+             (e.target as HTMLElement).classList.toggle("Note-Selected"); 
+             let out = WebMidi.getOutputByName(name) as Output
+             if(ourState.output?.id === out?.id){
+               outputSelection(null);
+             }
+             else {
+              outputSelection(out); 
+             }
+ 
+             } } /><span className="tooltiptext">{name}</span></div>
+         })}
+
+      </Box>
    
         
     </header>
-    <main>
+    <main className="App-header">
+      <Box title="CC Control">        
+        <button disabled={ ourState.output === null} onClick={ (e) => { ourState.output?.sendControlChange(0, 1, "all")          
+          .sendProgramChange(0,"all")
+          }}>Bank 2 Programm 1</button>
+      </Box>
       <Box title="Piano">
         <div className='pianorole'>
          { new ChannelScale(new ChannelNote(60),SemitoneSteps.Chromatic).notes().map((x,i) => (
-         <div key={i} className={x.isBlackKey()?'blackkey':'whitekey'}>
+         <div key={i} className={x.isBlackKey()?'blackkey':'whitekey'}
+          onMouseDown={ (e) => { ourState.output?.playNote( x.midiNote, "all", { duration: 10000 }) }}
+          onMouseUp={ (e) => { ourState.output?.stopNote( x.midiNote, "all") }} 
+         >
            {x.name()}
         </div>
          )) }
         </div>
       </Box>
       </main>
-      <footer>
+      <footer className="App-header">
       <Box title="Debug">
         <small>{ourState.noteOn !== null? "[" + ourState.noteOn.channel + "] " + printerify(ourState.noteOn?.note):"missing note"}</small>
       </Box>
